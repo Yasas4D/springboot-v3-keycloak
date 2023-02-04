9805963c3f28b7e9ef75bb0c4b5aac8b2ec7e8db
Yasas Sandeepa <yasassandeepa007@gmail.com>
2023-02-04 19:24:43 +0530

    Init project

---
+++ src/main/java/com/example/springbootkeycloak/SpringbootKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootkeycloak/SpringbootKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
